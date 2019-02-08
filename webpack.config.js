0bb6dd5439914bb25c38049338362471397690cf
Zhang, Rick (CW - Best Buy Canada) <riczhang@bestbuycanada.ca>
2019-02-07 16:22:20 -0800

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path=require('path');
+
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.jsx?$/, 
+                exclude: /node-modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
