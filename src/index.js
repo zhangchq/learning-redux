e9398c39bf726964b695460352acd22b13a248ce
Zhang, Rick (CW - Best Buy Canada) <riczhang@bestbuycanada.ca>
2019-02-15 10:25:14 -0800

    use ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
 import { createPost, editPost, setFilter } from './actions';
 import { createStore } from 'redux';
 import appReducer from './reduces';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-let store = createStore(appReducer);
 
-const root = document.getElementById('root');
-const render = () => {
-    root.innerHTML = '';
+// let store = createStore(appReducer);
 
-    const { posts } = store.getState();
-    posts.forEach((post, index) => {
-        const item = document.createElement('li');
-        const text = document.createTextNode(post.user + ' - ' + post.text);
-        item.addEventListener('click', ()  => {
-            store.dispatch(editPost(index, post.text + '!'));
-        })
-        item.appendChild(text);
-        root.appendChild(item);
-    })
-}
+// const root = document.getElementById('root');
+// const render = () => {
+//     root.innerHTML = '';
 
-const stopRender = store.subscribe(render);
+//     const { posts } = store.getState();
+//     posts.forEach((post, index) => {
+//         const item = document.createElement('li');
+//         const text = document.createTextNode(post.user + ' - ' + post.text);
+//         item.addEventListener('click', ()  => {
+//             store.dispatch(editPost(index, post.text + '!'));
+//         })
+//         item.appendChild(text);
+//         root.appendChild(item);
+//     })
+// }
 
-store.dispatch(createPost('rick', 'hello world'));
-store.dispatch(createPost('roth', 'second post'));
+// const stopRender = store.subscribe(render);
 
+// store.dispatch(createPost('rick', 'hello world'));
+// store.dispatch(createPost('roth', 'second post'));
+
+
+ReactDOM.render(
+    React.createElement('h1', {}, 'Hello world!'),
+    document.getElementById('root')
+);
 
