ef72f462ea905b9acb4a6d013db3b708bf4ec2d6
Zhang, Rick (CW - Best Buy Canada) <riczhang@bestbuycanada.ca>
2019-03-12 09:55:34 -0700

    set up redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { createPost, editPost, setFilter } from './actions';
-import { createStore } from 'redux';
 import appReducer from './reducers';
+import { createStore } from 'redux';
+import { createUser, createPost } from './actions';
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -8,9 +8,23 @@ import ConnectedPostList from "./containers/ConnectedPostList";
 
 let store = createStore(appReducer);
 
-store.dispatch(createPost("Rick", {title: "hi", text: "Hello world!"}));
+store.dispatch(createUser("rickz", "Rick Zhang"));
+store.dispatch(createUser("emily", "Emily"));
 
-setTimeout(() => store.dispatch(createPost("Emily", {title: "hi", text: "nice day"})), 1000);
+store.dispatch(createPost("rickz", {
+    title: "Hello",
+    text: "Hello world! first post",
+    category: "greeting",
+}));
+store.dispatch(createPost("emily", {
+    title: "test",
+    text: "Test, second post",
+    category: "test",
+}));
+
+console.log("initial state: ", store.getState());
+
+store.subscribe(() => console.log("state changed: ", store.getState()));
 
 const App = () => 
     <Provider store={store}>
