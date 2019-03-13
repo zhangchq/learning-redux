8fed79193f7f09116327cf0e9d9bac1fd12eec8f
Zhang, Rick (CW - Best Buy Canada) <riczhang@bestbuycanada.ca>
2019-03-13 14:51:24 -0700

    separate build script between production and development

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 const config = {
     entry: {
-        main: './src/index.js'
+        main: "./src/index.js"
     },
     output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/",
     },
     module: {
         rules: [
@@ -15,27 +16,32 @@ const config = {
                 test: /\.jsx?$/,
                 exclude: /node-modules/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: "babel-loader",
                     options: {
-                        presets: ['@babel/preset-env'],
-                        presets: ['@babel/preset-react'],
+                        presets: ["@babel/preset-env"],
+                        presets: ["@babel/preset-react"],
                     }
                 },
             }
         ],
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
-    }
+        extensions: [".js", ".jsx"],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development")
+        })
+    ]
 };
 
 module.exports = (env/*, args*/) => {
-    if (env.production) {
+    if (env && env.production) {
         config.mode = "production";
         config.devtool = 'source-map';
     } else {
         config.mode = "development";
-        config.devtool = 'cheap-module-eval-source-map';
+        config.devtool = "cheap-module-eval-source-map";
     }
 
     return config;
